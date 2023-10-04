5f1851e956542133ec693f31724617c3b70b6fe9
ErnestoDanielTafoyaMolina <tafoya.ernesto.1cm@gmail.com>
2023-10-03 19:50:45 -0600

    jwt middleware and lib ready

---
+++ src/libs/jwt.js
@@ -0,0 +1,11 @@
+import { secret } from "../config.js";
+import jwt from "jsonwebtoken";
+
+export async function createAccessToken( payload ) {
+  return new Promise((resolve, reject) => {
+    jwt.sign( payload, secret, { expiresIn: "1d" }, ( err, token ) => {
+      if ( err ) reject( err );
+      resolve( token );
+    });
+  });
+}
